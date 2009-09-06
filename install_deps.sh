1a34a60fd19866689e976eb646027a0404085dd1
Pedro Melo <melo@simplicidade.org>
2009-09-06 22:16:49 +0100

    Make sure the install_deps.sh script has the proper environment
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- install_deps.sh
+++ install_deps.sh
@@ -9,5 +9,8 @@ if [ -e "$distro_prefs" ] ; then
   cp -r prefs/ "$distro_prefs"
 fi
 
+export perl_version=`perl -MConfig -e 'print $Config{version}'`
+eval $(perl -I$HOME/.perl5/$perl_version/lib/perl5 -Mlocal::lib=$HOME/.perl5/$perl_version/)
+
 cd Task-Bootstrap/Task-Bootstrap-1.0
 PERL_MM_USE_DEFAULT=1 cpan .
