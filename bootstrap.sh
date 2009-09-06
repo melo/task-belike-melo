efa9c67589c7bca358c1c7e0e51aa13fb7e81532
Pedro Melo <melo@simplicidade.org>
2009-09-06 09:56:44 +0100

    Improve bootstrap.sh
     * splitted the installation of the dependencies to a new
       script, `install_deps.sh`: this allows you to run it standalone, after
       the first bootstrap;
     * After `Mac::Carbon` is installed, the location of the `.cpan/`
       directorty changes from `$HOME` to `$HOME/Library/Application Support`:
       we work around that, making sure that they both point to the same
       directory;
     * fix a bug with the local::lib install: we ended up in the wrong
       directory.
    
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,17 +1,29 @@
 #!/bin/sh
 
+export perl_version=`perl -MConfig -e 'print $Config{version}'`
+
 ## Make sure we have our local::lib
-cd local-lib
-perl Makefile.PL --bootstrap=~/.perl5/`perl -MConfig -e '$Config{version}'`
-make test && make install
+(
+  cd local-lib
+  perl Makefile.PL --bootstrap=~/.perl5/$perl_version
+  make test && make install
+)
+
 
 ## Ok, now use it to setup our environment
-export perl_version=`perl -MConfig -e 'print $Config{version}'`
 eval $(perl -I$HOME/.perl5/$perl_version/lib/perl5 -Mlocal::lib=$HOME/.perl5/$perl_version/)
+
+## After Mac::Carbon, the location of .cpan changes (!!)
+cpan_in_app_support="$HOME/Library/Application Support/.cpan"
+if [ -d "$cpan_in_app_support" ] ; then
+  mv "$cpan_in_app_support" "${cpan_in_app_support}.old"
+fi
+if [ ! -e "$cpan_in_app_support" ] ; then
+  ln -s $HOME/.cpan "$cpan_in_app_support"
+fi
 
 ## Run once to configure it
 cpan
 
 ## And now for our deps
-cd Task-Bootstrap/Task-Bootstrap-*
-PERL_MM_USE_DEFAULT=1 cpan .
+./install_deps.sh
---
+++ install_deps.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+cd Task-Bootstrap/Task-Bootstrap-1.0
+PERL_MM_USE_DEFAULT=1 cpan .
