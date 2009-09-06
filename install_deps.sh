e548342b170760cfcb765aab0dc2be1bd714a9d5
Pedro Melo <melo@simplicidade.org>
2009-09-06 10:17:07 +0100

    Make use of the CPAN distroprefs
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- install_deps.sh
+++ install_deps.sh
@@ -1,4 +1,13 @@
 #!/bin/sh
 
+distro_prefs="$HOME/.cpan/prefs"
+
+if [ ! -e "$distro_prefs" ] ; then
+  mkdir -p "$distro_prefs"
+fi
+if [ -e "$distro_prefs" ] ; then
+  cp -r prefs/ "$distro_prefs"
+fi
+
 cd Task-Bootstrap/Task-Bootstrap-1.0
 PERL_MM_USE_DEFAULT=1 cpan .
