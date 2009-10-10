6373d69fd97e1782f4a0972d0782c87b4635306e
Pedro Melo <melo@simplicidade.org>
2009-10-10 15:58:00 +0100

    Remove idiotic ignore line
    If we ignore the built version of the Task, we cannot bootstrap on
    systems that don't have Dist::Zilla already installed.
    
    Doh!
    
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

---
+++ Task-Bootstrap/Task-Bootstrap-1.0/lib/Task/Bootstrap.pm
@@ -0,0 +1,31 @@
+package Task::Bootstrap;
+our $VERSION = '1.0';
+
+
+
+use strict;
+use warnings;
+
+1;
+
+=head1 NAME
+
+Task::Bootstrap - All the modules I want
+
+=head1 VERSION
+
+version 1.0
+
+=head1 SYNOPSYS
+
+    ./bootstrap.sh
+    # Now grab a cup of tea, this will take a while..
+
+=head1 DESCRIPTION
+
+This task will install all the modules that I usually like to have around,
+in one fell swoop.
+
+It will take a while.
+
+=cut
