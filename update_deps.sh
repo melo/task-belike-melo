4f3f8fb968e2294e900a4572f140095729e4a3e3
Pedro Melo <melo@simplicidade.org>
2010-02-25 09:55:50 +0000

    Use the local git version of dist-zilla, with our prereq patches
    Signed-off-by: Pedro Melo <melo@simplicidade.org>

--- update_deps.sh
+++ update_deps.sh
@@ -6,7 +6,7 @@ export perl_version=`perl -MConfig -e 'print $Config{version}'`
 eval $(perl -I$HOME/.perl5/$perl_version/lib/perl5 -Mlocal::lib=$HOME/.perl5/$perl_version/)
 
 cd Task-Bootstrap
-dzil build
+PERL5LIB=/Users/melo/work/projects/perl/dist-zilla/core/lib:$PERL5LIB dzil build
 
 cd Task-Bootstrap-1.0
 PERL_MM_USE_DEFAULT=1 cpan .
